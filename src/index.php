ebf764c03bc09fd5e0166a9066a4c29c632c4ee4
madushankaf <madushankafonseka@live.com>
2021-04-06 22:17:40 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello, World from Docker !!!! <br>";
+echo "Hello, World from Gitops !!!! <br>";
 echo "Hello ECS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
